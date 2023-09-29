b00c2b01d23d396dcf4de8f10acd381dd8cf0141
fredrikaaa <fredrik00@pm.me>
2023-09-29 15:44:12 +0200

    Added basic insert_data function.

--- src/SqlQueries.py
+++ src/SqlQueries.py
@@ -1,11 +1,11 @@
 
 
-def create_tables(db_connector, table_definition):
+def create_tables(db_connector):
     table_names = ["User", "Activity", "TrackPoint"]
     table_definitions = [
-        "id VARCHAR(255), has_labels boolean",
-        "id INT AUTO_INCREMENT, FOREIGN KEY (user_id) references USER(id), transportation_mode VARCHAR(255), start_date_time DATETIME, end_date_time DATETIME",
-        "id INT AUTO_INCREMENT, FOREIGN KEY (activity_id) references Activity(id), lat DOUBLE, lon DOUBLE, altitude INT, date_days DOUBLE, date_time DATETIME"
+        "user_id VARCHAR(255), has_labels boolean",
+        "activity_id INT AUTO_INCREMENT, FOREIGN KEY (user_id) references USER(user_id), transportation_mode VARCHAR(255), start_date_time DATETIME, end_date_time DATETIME",
+        "trackpoint_id INT AUTO_INCREMENT, FOREIGN KEY (activity_id) references Activity(activity_id), lat DOUBLE, lon DOUBLE, altitude INT, date_days DOUBLE, date_time DATETIME"
     ]
     try:
         for table_name, table_definition in table_names, table_definitions:
@@ -14,3 +14,15 @@ def create_tables(db_connector, table_definition):
             print(f"Table '{table_name}' created successfully.")
     except Exception as e:
         print(f"Error creating table '{table_name}': {e}")
+
+
+def insert_data(db_connector, table_name, data):
+    try:
+        db_connector.start_
+        insert_query = f"INSERT INTO {table_name} VALUES ({data})"
+        db_connector.cursor.execute(insert_query)
+        db_connector.db_connection.commit()
+        print(f"Data inserted into table '{table_name}' successfully.")
+    except Exception as e:
+        print(f"Error inserting data into table '{table_name}': {e}")
+        db_connector.db_connection.rollback()
